478e7489e49ff3753aed9326a450185e7e797048
nugunga <nugunga293@gmail.com>
2022-05-26 22:10:55 +0900

    1

--- Ex1_7.java
+++ Ex1_7.java
@@ -4,10 +4,12 @@ public class Ex1_7 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);        
 
-        System.out.println("숫자를 입력하세요");
-        int num1 = scanner.nextInt();
-        System.out.println("더할 숫자를 입력하세요");
-        int num2 = scanner.nextInt();
+        System.out.println("숫자를 입력하세요"); 
+        int num1 = scanner.nextInt(); //숫자 입력1
+
+        System.out.println("더할 숫자를 입력하세요"); 
+        int num2 = scanner.nextInt(); //숫자 입력2
+
         System.out.println("값은 : " + (num1 + num2));
 
      
